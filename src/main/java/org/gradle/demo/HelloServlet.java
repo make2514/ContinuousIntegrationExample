aac9dc1473ba2e090657eaeb478898363b3878f7
Toan Nguyen <toan.nguyen@frosmo.com>
2018-04-17 11:11:16 +0300

    Connect to heroku mysql proof of concept

--- src/main/java/org/gradle/demo/HelloServlet.java
+++ src/main/java/org/gradle/demo/HelloServlet.java
@@ -6,12 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(name = "HelloServlet", urlPatterns = {"hello"}, loadOnStartup = 1) 
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        response.getWriter().print("Hello, World!");  
+        try {
+            Connection connection = getConnection();
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+            stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+            while (rs.next()) {
+                System.out.println("Read from DB: " + rs.getTimestamp("tick"));
+                response.getWriter().print(rs.getTimestamp("tick"));  
+            }
+        } catch (URISyntaxException | SQLException ex) {
+            Logger.getLogger(HelloServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -20,5 +38,15 @@ public class HelloServlet extends HttpServlet {
         if (name == null) name = "World";
         request.setAttribute("user", name);
         request.getRequestDispatcher("response.jsp").forward(request, response); 
+    }
+    
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI("DATABASE_URL");
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
